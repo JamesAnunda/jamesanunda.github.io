7310d0b34b3cbd3f35e68d3fa36ec111ad8b42c6
JamesAnunda <cjanunda@gmail.com>
2019-02-08 23:26:29 -0500

    action.php revamp
    Remove error line for the forum
    Start revamp ther action

--- assests/php/action.php
+++ assests/php/action.php
@@ -1,19 +1,18 @@
 <?php
 if(isset($_POST['email'])) {
 
-    // EDIT THE 2 LINES BELOW AS REQUIRED
+    // address and subject line
     $email_to = "cjanunda@gmail.com";
     $email_subject = "How can I help you?";
 
     function died($error) {
-        // your error code can go here
+        //error code can go here
         echo "We are very sorry, but there were error(s) found with the form you submitted. ";
         echo "These errors appear below.<br /><br />";
         echo $error."<br /><br />";
         echo "Please go back and fix these errors.<br /><br />";
         die();
     }
-
     // validation expected data exists
     if(!isset($_POST['Name']) ||
         !isset($_POST['Email']) ||
@@ -21,10 +20,9 @@ if(isset($_POST['email'])) {
         died('We are sorry, but there appears to be a problem with the email you submitted.');
     }
 
-    $Name = $_POST['Name']; // required
-    $email_from = $_POST['Email']; // required
-    $comments = $_POST['Message']; // required
-
+    $Name = $_POST['Name']; 
+    $email_from = $_POST['Email']; 
+    $comments = $_POST['Message']; 
     $error_message = "";
     $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
 
@@ -33,22 +31,12 @@ if(isset($_POST['email'])) {
   }
 
     $string_exp = "/^[A-Za-z .'-]+$/";
-
-  if(!preg_match($string_exp,$Name)) {
-    $error_message .= 'The Name you entered does not appear to be valid.<br />';
-  }
-
+    
   if(strlen($error_message) > 0) {
     died($error_message);
   }
 
     $email_message = "Form details below.\n\n";
-
-    function clean_string($string) {
-      $bad = array("content-type","bcc:","to:","cc:","href");
-      return str_replace($bad,"",$string);
-    }
-
     $email_message .= "Name: ".clean_string($Name)."\n";
     $email_message .= "Email: ".clean_string($email_from)."\n";
     $email_message .= "Message: ".clean_string($Message)."\n";
